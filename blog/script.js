0913f7e43bf088aa06875f5a01ed43dbafab46ba
Zagura <me@zagura.one>
2023-03-27 16:11:35 +0200

    eee

--- blog/script.js
+++
@@ -1,7 +0,0 @@
-
-fetch("https://webmention.io/api/count?target=https://zagura.one")
-    .then(response => response.json())
-    .then(responseJson => {
-        let html = `<p>Webmentions: ${responseJson.count}</p>`;
-        document.body.querySelector("#mentions").insertAdjacentHTML("beforeend", html);
-    });
