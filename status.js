18ba2f223c68a5c15f4ec793ff4d81363fb717c6
Zagura <me@zagura.one>
2023-07-25 19:04:55 +0200

    a

--- status.js
+++ status.js
@@ -5,6 +5,6 @@ fetch("https://status.cafe/users/zagura/status.json")
       document.getElementById("statuscafe-content").innerHTML = "No status yet"
       return
     }
-    document.getElementById("statuscafe-header").innerHTML = 'on <a href=https://status.cafe/users/zagura target=_blank>Status Cafe</a>, ' + r.timeAgo
+    document.getElementById("statuscafe-header").innerHTML = r.face + ' on <a href=https://status.cafe/users/zagura target=_blank>Status Cafe</a>, ' + r.timeAgo
     document.getElementById("statuscafe-content").innerHTML = r.content
   })
