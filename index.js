52f534e495ad4ba039232227acec33e7e4f790ea
Zagura <me@zagura.one>
2023-10-27 22:51:32 +0200

    a

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ fetch("https://status.cafe/users/zagura/status.json")
     if (!r.content.length)
       return;
     document.getElementById("statuscafe-face").innerHTML = r.face;
-    document.getElementById("statuscafe").innerHTML = `<article id=statuscafe class=status>
-      <div class=status-header>on <a class=chill href=https://status.cafe/users/zagura target=_blank>Status Cafe</a>, ${r.timeAgo}</div>
+    document.getElementById("statuscafe").innerHTML = `<hr><div id=statuscafe class=status>
+      <div class=status-header>on <a href=https://status.cafe/users/zagura target=_blank>Status Cafe</a>, ${r.timeAgo}</div>
       <div class=status-content>${r.content}</div>
-      </article>`;
+      </div>`;
   });
 
 fetch("https://tech.lgbt/api/v1/accounts/109155032729615779/statuses?exclude_replies=true&exclude_reblogs=true")
   .then(r => r.json())
   .then(r => {
     const post = r[0];
-    document.getElementById("fedi").innerHTML = `<article id=fedi class=status>
-      <div class=status-header>on <a class=chill href="${post.url}" target=_blank>tech.lgbt</a>, ${new Date(r[0].created_at).toLocaleDateString(undefined, {
+    document.getElementById("fedi").innerHTML = `<hr><div id=fedi class=status>
+      <div class=status-header>on <a href="${post.url}" target=_blank>tech.lgbt</a>, ${new Date(r[0].created_at).toLocaleDateString(undefined, {
         year: 'numeric',
         month: 'long',
         day: 'numeric',
       })}</div>
       <div id=fedi-content class=status-content>${post.content}</div>
-      </article>`;
+      </div>`;
   });
